964359e795b4105e8395033c90fae3c1c0247cf6
Mai7ei <375336062768@ya.ru>
2019-06-18 17:07:30 +0300

    Test3
    new

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,7 +1,29 @@
 $(document).ready(function(){
 
-	$('.services_slider').owlCarousel({
-		items:4
+	$('.services_slider').slick({
+		infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+    	 appendDots: $(".slide-m-dots_develop"),
+    prevArrow: $(".slick-prev_develop"),
+    nextArrow: $(".slick-next_develop")
 	});
 
+
+			$(".info-wrapper .collapse").on('show.bs.collapse', function() {
+      $(this).closest(".info-wrapper").find(".burger-menu").addClass("menu-on");
+    });
+			$(".info-wrapper .collapse").on('hide.bs.collapse', function() {
+      $(this).closest(".info-wrapper").find(".burger-menu").removeClass("menu-on");
+    });
+
+
+	// $('.info-btn').click(function(){
+	// 	$('.burger-menu').toggleClass('menu-on');
+
+	// });
+
+
 });
+
